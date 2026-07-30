61ac717a726f7bd20a748bd3e3bb5fce3cdf180b
amanbti143-hub <amanbti143@gmail.com>
2026-07-31 00:08:38 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,6 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // RepositoriesMode change karke PREFER_SETTINGS kiya gaya hai taaki build fail na ho
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
